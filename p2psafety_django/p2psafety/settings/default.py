ba774c21dc8aa080237ccef6fc7d1f1586f0135f
Alex Vykalyuk <alekzvik@gmail.com>
2014-01-21 21:49:18 +0200

    ticket:24 Added users app with custom apikey generation

--- p2psafety_django/p2psafety/settings/default.py
+++ p2psafety_django/p2psafety/settings/default.py
@@ -81,3 +81,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TASTYPIE_ABSTRACT_APIKEY = True
---
+++ p2psafety_django/users/models.py
@@ -0,0 +1,58 @@
+import uuid
+import hmac
+
+from django.db import models
+from django.contrib.auth.models import User
+
+from tastypie.models import ApiKey
+
+try:
+    from hashlib import sha1
+except ImportError:
+    import sha
+    sha1 = sha.sha
+
+
+class PIN(ApiKey):
+    """
+    ApiKey model for api auth. Basically like tasypie's, but with custom
+    generator and active/passive state.
+    """
+    user = models.ForeignKey(User, related_name='api_key')
+    is_active = models.BooleanField(default=True)
+    PIN = models.SmallIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        """
+        Basic save + generator until PIN is unique between active ones.
+        """
+        if not self.key:
+            bad_key = True
+            while bad_key:
+                self.PIN, self.key = self.generate_key()
+                bad_key = self.objects.filter(PIN=self.PIN).exists()
+
+        return super(PIN, self).save(*args, **kwargs)
+
+    def generate_key(self):
+        """
+        Generates uuid, and PIN.
+        """
+        new_uuid = uuid.uuid4()
+        PIN = new_uuid.int % 1000000
+        key = hmac.new(new_uuid.bytes, digestmod=sha1).hexdigest()
+        return (PIN, key)
+
+    @classmethod
+    def regenerate(cls, user):
+        """
+        Invalidates old keys for user and creates a new one.
+        """
+        old_keys = cls.objects.filter(user=user, is_active=True)
+        for key in old_keys:
+            key.is_active = False
+            key.save()
+        cls.objects.create(user=user)
+
+
+
