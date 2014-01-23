e5d388b9bd8d458f594c00cb667cbefbb2da81c4
Alex Vykalyuk <alekzvik@gmail.com>
2014-01-23 13:28:54 +0200

    ticket:25 Added validation for EventResource

--- p2psafety_django/events/api/resources.py
+++ p2psafety_django/events/api/resources.py
@@ -1,12 +1,32 @@
+# -*- coding: utf-8 -*-
 from django.conf import settings
 
+from tastypie import http
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
+from tastypie.validation import Validation
+from tastypie.exceptions import ImmediateHttpResponse
+
 from social.backends.utils import get_backend
 from social.apps.django_app.utils import load_strategy
 
 from events.models import Event, EventUpdate
+
+
+class EventValidation(Validation):
+    def is_valid(self, bundle, request=None):
+        if not bundle.data:
+            return {'__all__': 'Please add provider and access_token arguments'}
+
+        errors = {}
+
+        provider = bundle.data.get('provider')
+        backend = get_backend(settings.AUTHENTICATION_BACKENDS, provider)
+        if not backend:
+            errors['provider'] = 'This provider is not supported'
+
+        return errors
 
 
 class EventResource(ModelResource):
@@ -15,6 +35,7 @@ class EventResource(ModelResource):
         resource_name = 'events'
         authentication = Authentication()
         authorization = Authorization()
+        validation = EventValidation()
         list_allowed_methods = ['post', ]
         detail_allowed_methods = []
         always_return_data = True
@@ -22,8 +43,6 @@ class EventResource(ModelResource):
     def hydrate(self, bundle):
         access_token = bundle.data.get('access_token')
         provider = bundle.data.get('provider')
-        if not (provider and access_token):
-            pass  # Add some error message + check if provider in AVAILABLE_PROVIDERS
 
         social_auth_backend = get_backend(settings.AUTHENTICATION_BACKENDS, provider)
 
@@ -36,7 +55,8 @@ class EventResource(ModelResource):
                 user = social_auth.do_auth(access_token)
                 bundle.obj.user = user
             except:
-                pass  # Invalid user token
+                raise ImmediateHttpResponse(
+                    response=http.HttpBadRequest('Invalid access token'))
             bundle.data.pop('provider')
             bundle.data.pop('access_token')
 
@@ -54,5 +74,7 @@ class EventUpdateResource(ModelResource):
         key = bundle.data.get('key')
         event = Event.objects.get(key=key)
         bundle.obj.event = event
+
+        # Add hydration for location. It must receive latitude and longitude
 
         return bundle
