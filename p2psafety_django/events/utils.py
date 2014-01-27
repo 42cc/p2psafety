fc82768686d7dc56c2c2a49728aa3f7400a0ff8a
Alex Vykalyuk <alekzvik@gmail.com>
2014-01-27 22:33:20 +0200

    ticket:25 Added docstring for helper

--- p2psafety_django/events/utils.py
+++ p2psafety_django/events/utils.py
@@ -2,4 +2,7 @@
 
 
 def geo_point(latitude=None, longitude=None):
+    """
+    Helper to make geo point and not confuse the order of lat. and long.
+    """
     return 'POINT (%f %f)' % (float(longitude), float(latitude))
