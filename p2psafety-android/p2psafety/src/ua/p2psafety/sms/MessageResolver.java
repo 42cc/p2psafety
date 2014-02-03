474776ed6bf9711072f1689f61690b6b750cbda0
UncleVasya (Oleg Ovcharenko) <Oleg_Ovcharenko@list.ru>
2014-02-03 14:51:56 +0200

    ticket:31 combine two alert message into one; small cleanup

--- p2psafety-android/p2psafety/src/ua/p2psafety/sms/MessageResolver.java
+++ p2psafety-android/p2psafety/src/ua/p2psafety/sms/MessageResolver.java
@@ -27,13 +27,11 @@ public class MessageResolver {
     private List<String> phones;
     private List<String> emails;
 
-    public MessageResolver(Context context, boolean isTest) {
+    public MessageResolver(Context context) {
         this.context = context;
         PhonesDatasourse phonesDatasourse = new PhonesDatasourse(context);
         EmailsDatasourse emailsDatasourse = new EmailsDatasourse(context);
         message = Prefs.getMessage(context);
-        if (isTest)
-            message = context.getString(R.string.test_message) + " " + message;
         phones = new ArrayList<String>();
         phones = phonesDatasourse.getAllPhones();
         emails = emailsDatasourse.getAllEmails();
@@ -58,76 +56,58 @@ public class MessageResolver {
         }
     }
 
+    // TODO: refactor this code or better whole MessageResolver
+    // (split it into SMSSender & EmailSender?)
     public void sendMessages() {
         AsyncTask ast = new AsyncTask() {
             @Override
             protected Object doInBackground(Object[] params) {
                 try {
-                    Log.d("Message1", message);
-                    sendMessage(message);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Log.e("Error", "while sending SMS ", e);
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Object o) {
-                super.onPostExecute(o);
-                String s = context.getString(R.string.sms_sent);
-//                if (Prefs.getIsLoc(context))
-//                    s += "\n" + context.getString(R.string.loc_will_send);
-                Toast.makeText(context, s, Toast.LENGTH_LONG).show();
-            }
-        };
-        try {
-            ast.execute();
-        } catch (Exception e) {
-        }
-        if (Prefs.getIsLoc(context)) {
-
-            try {
-
-                AsyncTask astGeo = new AsyncTask() {
-                    @Override
-                    protected Object doInBackground(Object[] params) {
+                    if (Prefs.getIsLoc(context)) {
+                        // loop until we have location
                         Looper.prepare();
                         MyLocation.LocationResult locationResult = new MyLocation.LocationResult() {
                             @Override
                             public void gotLocation(Location location) {
-                                if (location!=null)
-                                {
+                                if (location != null) {
                                     String lat = location.getLatitude() + "";
                                     if (lat.length() > 10)
                                         lat = lat.substring(0, 9);
                                     String lon = location.getLongitude() + "";
                                     if (lon.length() > 10)
                                         lon = lon.substring(0, 9);
-                                    message = formatTimeAndDay(location.getTime(), false) + " https://maps.google.com/maps?q="
-                                            + lat + "," + lon;
-                                    Log.d("Message1", "Message sent" + message);
+
+                                    Log.i("locationResult", "message: " + message);
+
+                                    message = new StringBuilder().append(message)
+                                            .append("\n")
+                                            .append(formatTimeAndDay(location.getTime(), false))
+                                            .append(" https://maps.google.com/maps?q=")
+                                            .append(lat).append(",").append(lon).toString();
+
+                                    Log.i("locationResult", "message: " + message);
+
                                     sendMessage(message);
+                                    Log.d("Message", "1.0 Message sent" + message);
                                 }
                             }
                         };
                         MyLocation myLocation = new MyLocation();
                         myLocation.getLocation(context, locationResult);
                         Looper.loop();
-
-                        return null;
                     }
-                };
-                if (android.os.Build.VERSION.SDK_INT < 11)
-                    astGeo.execute();
-                else
-                    astGeo.executeOnExecutor(Executors.newCachedThreadPool());
-            } catch (Exception e) {
-                Toast.makeText(context, R.string.sms_failed, Toast.LENGTH_LONG).show();
+
+                    //sendMessage(message);
+                    //Log.d("Message", "1.1 Message sent" + message);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.e("Error", "while sending messages ", e);
+                }
+                return null;
             }
-
-        }
+        };
+        try {
+            ast.execute();
+        } catch (Exception e) {}
     }
-
 }
