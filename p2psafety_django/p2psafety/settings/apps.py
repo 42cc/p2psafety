18eb84a059c190741e668f5d9ca4548b78b28d8f
Ferens Dmitriy <ferensdima@gmail.com>
2014-02-12 01:40:45 +0200

    ticket:51 Added ACCOUNT_MODERATION_REQUIRED setting

--- p2psafety_django/p2psafety/settings/apps.py
+++ p2psafety_django/p2psafety/settings/apps.py
@@ -1,23 +1,36 @@
+#
+# Site settings
+#
+# Set ``is_active`` to False for new registered users or not
+ACCOUNT_MODERATION_REQUIRED = False
+
+#
+# Tastypie settings
+#
 TASTYPIE_ABSTRACT_APIKEY = True
 API_LIMIT_PER_PAGE = 0
-
-SRID = {
-    'default': 4326,  # WGS84, stored in database
-    'projected': 900913,  # for Spatialite distance calculation
-}
 
 #
 # django-allauth settings
 #
+
 # Login method to use
 ACCOUNT_AUTHENTICATION_METHOD = 'username'
+
 # Required during registration
 ACCOUNT_EMAIL_REQUIRED = True
+
 # Email verification is required
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_FORM_CLASS = 'users.forms.SignupForm'
 
-
+#
+# PostGIS settings
+#
+SRID = {
+    'default': 4326,  # WGS84, stored in database
+    'projected': 900913,  # for Spatialite distance calculation
+}
 POSTGIS_TEMPLATE = 'template_postgis'
 
 GOOGLE_API_KEY = NotImplemented
--- p2psafety_django/users/forms.py
+++ p2psafety_django/users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 
 from .models import Profile
 
@@ -17,3 +18,7 @@ class SignupForm(forms.ModelForm):
     def save(self, user):
         user.profile.phone_number = self.cleaned_data['phone_number']
         user.profile.save()
+
+        if settings.ACCOUNT_MODERATION_REQUIRED:
+            user.is_active = False
+            user.save()
