6b8340d5a0497ef9d3d39d3646d633e588eef4a8
Ferens Dmitriy <ferensdima@gmail.com>
2014-02-20 17:26:04 +0200

    ticket:54 Fixed devdata command

--- p2psafety_django/events/management/commands/devdata.py
+++ p2psafety_django/events/management/commands/devdata.py
@@ -2,6 +2,7 @@ import datetime
 import random
 
 from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
 from django.contrib.gis import geos
 
 from events.tests.helpers.factories import EventFactory, EventUpdateFactory
@@ -16,7 +17,7 @@ class Command(BaseCommand):
 
     def setup(self):
         data = {
-            'main_user': UserFactory(username="devdata_user"),
+            'main_user': User.objects.get_or_create(username="devdata_user")[0],
             'timestamp_start': datetime.datetime.now(),
             'location_start': geos.Point(390.56, 50.43),
         }
