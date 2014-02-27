42ff0db3cca9a75c21d73794eab78c328cf80d0b
Ferens Dmitriy <ferensdima@gmail.com>
2014-02-27 22:17:27 +0200

    ticket:35 Improved tests

--- p2psafety_django/events/tests/test_api/test_resources_jabber.py
+++ p2psafety_django/events/tests/test_api/test_resources_jabber.py
@@ -18,4 +18,20 @@ class EventTestCase(ResourceTestCase):
         data = resource.full_dehydrate(bundle).data
         self.assertIsInstance(data.get('support'), basestring)
         self.assertIsInstance(data.get('radius'), numbers.Number)
+
+    def test_has_location(self):
+        event = EventFactory()
+        event_update = EventUpdateFactory(event=event, location=Point(1, 1))
+
+        resource = EventResource()
+        bundle = resource.build_bundle(obj=event)
+        data = resource.full_dehydrate(bundle).data
         self.assertEqual(data.get('location'), {'longitude': 1, 'latitude': 1})
+
+    def test_no_location(self):
+        event = EventFactory()
+
+        resource = EventResource()
+        bundle = resource.build_bundle(obj=event)
+        data = resource.full_dehydrate(bundle).data
+        self.assertEqual(data.get('location'), None)
