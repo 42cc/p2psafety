8782940363d87112facb9449d544fac5ff2ed8c1
Alex Vykalyuk <alekzvik@gmail.com>
2014-03-04 19:53:50 +0200

    ticket:68 Fixed tests

--- p2psafety_django/events/tests/test_switches.py
+++ p2psafety_django/events/tests/test_switches.py
@@ -8,12 +8,12 @@ class EventSwitchesTestCase(SwithcesTestCase):
 
     @mock.patch('events.models.jabber')
     def test_supporters_autonotify(self, mock_jabber):
-        event = EventFactory()
+        event1, event2 = EventFactory(), EventFactory()
 
         self.set_switch('supporters-autonotify', False)
-        EventUpdateFactory(event=event)
+        EventUpdateFactory(event=event1)
         self.assertFalse(mock_jabber.notify_supporters.called)
 
         self.set_switch('supporters-autonotify')
-        EventUpdateFactory(event=event)
+        EventUpdateFactory(event=event2)
         self.assertEqual(mock_jabber.notify_supporters.call_count, 1)
