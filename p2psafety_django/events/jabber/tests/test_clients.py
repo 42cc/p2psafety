ff3c7bf6ad336c0411c927749f6238d4f239de79
Ferens Dmitriy <ferensdima@gmail.com>
2014-03-11 23:00:12 +0200

    ticket:72 Added payload tests & helpers

---
+++ p2psafety_django/events/jabber/tests/test_clients.py
@@ -0,0 +1,36 @@
+import mock
+import contextlib
+
+from django.test import TestCase
+
+from sleekxmpp.xmlstream import ET
+from lxml import etree
+
+from .helpers import MockedEventsNotifierClient
+from .. import clients
+from ...tests.helpers.factories import EventFactory
+from users.tests.helpers import UserFactory
+
+
+class EventsNotifierTestCase(TestCase):
+
+    def test_publish(self):
+        user = UserFactory()
+        event = EventFactory(user=user)
+        client = MockedEventsNotifierClient()
+        payload = ("""
+            <object>
+              <text type="null"></text>
+              <support>/api/v1/events/1/support/</support>
+              <radius type="integer">123</radius>
+              <user>
+                <id type="integer">1</id>
+                <full_name>%s</full_name>
+              </user>
+              <location type="null"/>
+            </object>
+            """ % (user.username,)
+        )
+
+        client.publish(event, 123)
+        client.assert_published_once_with(payload)
