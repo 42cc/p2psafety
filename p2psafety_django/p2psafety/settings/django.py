09da85e1b7ef56141f3b596537274b91f452d401
Alex Vykalyuk <alekzvik@gmail.com>
2014-03-29 19:47:05 +0200

    ticket:00 Changed default time zone to UTC+2

--- p2psafety_django/p2psafety/settings/django.py
+++ p2psafety_django/p2psafety/settings/django.py
@@ -24,7 +24,7 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
